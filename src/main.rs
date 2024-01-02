7cbb6f530b43f099a597d651e2a19208f4fecb07
Nikita0x <klirmio21@gmail.com>
2024-01-02 22:17:50 +0200

    added basic camera + a plane scene for the camera to look at

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 use bevy::prelude::*;
 
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .run();
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Startup, spawn_basic_scene)
+        .run(); 
+}
+
+
+// spawn camera
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default() 
+    });
+}
+
+// adding basic scene to look at  (Pbr stands for - Physically Based Rendering)
+fn spawn_basic_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    // mut materials: ResMut<Asset<StandardMaterial>>,
+) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane {size: 5.0, subdivisions: 0}
+        )),
+        ..default()
+    });
 }
