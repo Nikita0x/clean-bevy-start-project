32a975afa49153b2d7cf4d6fb4526bb70965d9b7
Nikita0x <klirmio21@gmail.com>
2024-01-04 18:52:20 +0200

    Added WorldInspector

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use bevy::prelude::*;
-
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn_camera)
         .add_systems(Startup, spawn_basic_scene)
+        .add_plugins(WorldInspectorPlugin::new())
         .run(); 
 }
 
@@ -15,7 +16,8 @@ fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default() 
-    });
+    })
+    .insert(Name::new("Camera"));
 }
 
 // adding basic scene to look at
@@ -24,16 +26,32 @@ fn spawn_basic_scene(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>
 ) {
+
+    // adding Plane
     commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Plane {size: 5.0, subdivisions: 0}
         )),
         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         ..default()
-    });
+    })
+    .insert(Name::new("Ground"));
+    // adding Cube
     commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Cube {size: 1.0})),
         material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..default()
-    });
+    })
+    .insert(Name::new("Cube"));
+    // adding light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    })
+    .insert(Name::new("Light"));
 }
